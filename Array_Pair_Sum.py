051800762828e0e06ebd2bc9c316ab12968b023f
Ozkan Batumoglu <batumoglu@hotmail.com>
2019-12-30 15:41:25 +0000

    Array Pair Sum Added

---
+++ Array_Pair_Sum.py
@@ -0,0 +1,16 @@
+def pair_sum(arr, k):
+    seen = set()
+    output = set()
+
+    for num in arr:
+        target = k - num
+
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add((min(num, target), max(num, target)))
+            seen.remove(target)
+    return(len(output))
+
+count = pair_sum([1,3,2,2,],4)
+print(count)
