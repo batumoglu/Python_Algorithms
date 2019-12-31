ae290af11ec0d162b3c0fc286d608430ef299d06
Ozkan Batumoglu <batumoglu@hotmail.com>
2019-12-31 19:50:22 +0000

    Array Pair Updated

--- Array_Pair_Sum.py
+++ Array_Pair_Sum.py
@@ -4,12 +4,23 @@ def pair_sum(arr, k):
 
     for num in arr:
         target = k - num
-
-        if target not in seen:
-            seen.add(num)
-        else:
-            output.add((min(num, target), max(num, target)))
+        if target in seen:
             seen.remove(target)
-    print(len(output))
+            output.add((num,target))
+        else:
+            seen.add(num)
+    return len(output)
 
-pair_sum([1,3,2,2,],4)
+from nose.tools import assert_equal
+
+class TestPair(object):
+    
+    def test(self,sol):
+        assert_equal(sol([1,9,2,8,3,7,4,6,5,5,13,14,11,13,-1],10),6)
+        assert_equal(sol([1,2,3,1],3),1)
+        assert_equal(sol([1,3,2,2],4),2)
+        print('ALL TEST CASES PASSED')
+
+if __name__ == '__main__':
+    t = TestPair()
+    t.test(pair_sum)
