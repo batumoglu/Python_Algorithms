6497c89b85d92cb94ddbd339bc7fa7d322cd7042
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-05 09:56:34 +0000

    Merge Two Sorted List added

---
+++ LeetCode/MergeTwoSortedList.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        
+        head = tail = ListNode(None)
+
+        while l1 and l2:
+            if l1.val >= l2.val:
+                tail.next = l2
+                l2 = l2.next
+            else:
+                tail.next = l1
+                l1 = l1.next
+            tail = tail.next
+
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return head.next
