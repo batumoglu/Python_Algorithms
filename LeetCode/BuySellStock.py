638e9336e62018cc8dd753616a6674cc204c8ce5
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-06 10:40:15 +0000

    Buy Sell Stock Added

---
+++ LeetCode/BuySellStock.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+
+
+        for day, price in enumerate(prices):
+
+
+        if __name__ == '__main__':
+            sol = Solution()
+            print(sol.maxProfit([7,1,5,3,6,4]))
