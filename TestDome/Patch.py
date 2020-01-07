f5c26fbbd8b55c861bf5552428baf25690620c01
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-07 21:39:09 +0000

    Update on Patch

--- TestDome/Patch.py
+++ TestDome/Patch.py
@@ -2,9 +2,13 @@ import math
 
 def add_patch():
     # Write the code that goes here
-    math.log100 = math.log(self, 100)
+    module = __import__("math")
+    def log100(num):
+        return math.log(num, 100)
+        setattr(module, "log100", log100)
         
 # Example case.
 # add_patch()      
 # print(math.log100(10))
 print(add_patch())
+print(math.log100(10))
