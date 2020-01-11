f6a266db79b90fbe28fa316ff776b22e9af3970f
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-11 13:19:25 +0000

    Fast Solution Added

--- Codility/MinAvgTwoSlice.py
+++ Codility/MinAvgTwoSlice.py
@@ -13,5 +13,19 @@ def solution(A):
             count = i
     return count
 
+def solution2(A):
+    min_idx = 0
+    min_value = float('inf')
+ 
+    for idx in range(0, len(A)-1):
+        if (A[idx] + A[idx+1])/2.0 < min_value:
+            min_idx = idx
+            min_value = (A[idx] + A[idx+1])/2.0
+        if idx < len(A)-2 and (A[idx] + A[idx+1] + A[idx+2])/3.0 < min_value:
+            min_idx = idx
+            min_value = (A[idx] + A[idx+1] + A[idx+2])/3.0
+ 
+    return min_idx
+
 if __name__ == '__main__':
     print(solution2([4,2,2,5,1,5,8]))
