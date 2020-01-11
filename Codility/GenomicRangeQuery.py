b29c8fd60223d070b3bb05d678e20c77908d42bc
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-11 11:32:18 +0000

    Solution2 added, if x in str

--- Codility/GenomicRangeQuery.py
+++ Codility/GenomicRangeQuery.py
@@ -14,6 +14,19 @@ def solution(S, P, Q):
         res[i] = min(values[P[i]:Q[i]+1])
     return res
 
+def solution2(S, P, Q):
+    res = []
+    for i in range(len(P)):
+        if 'A' in S[P[i]:Q[i]+1]:
+            res.append(1)
+        elif 'C' in S[P[i]:Q[i]+1]:
+            res.append(2)
+        elif 'G' in S[P[i]:Q[i]+1]:
+            res.append(3)
+        else:
+            res.append(4)
+    return res
+
     
 if __name__ == '__main__':
     print(solution('CAGCCTA', [2,5,0], [4,5,6]))
