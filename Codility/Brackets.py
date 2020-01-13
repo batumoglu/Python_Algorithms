6f48a65aab8459606c7488c3f1833d341082e6cc
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-13 14:24:44 +0000

    append, pop

--- Codility/Brackets.py
+++ Codility/Brackets.py
@@ -18,6 +18,53 @@ def solution(S):
     else:
         return 0
 
+def isValidPair(left, right):
+    if left == '(' and right == ')':
+        return True
+    if left == '[' and right == ']':
+        return True 
+    if left == '{' and right == '}':
+        return True   
+    return False
+ 
+def solution2(S):
+    stack = []
+     
+    for symbol in S:
+        if symbol == '[' or symbol == '{' or symbol == '(':
+            stack.append(symbol)
+        else:
+            if len(stack) == 0:
+                return 0
+            last = stack.pop()
+            if not isValidPair(last, symbol):
+                return 0
+     
+    if len(stack) != 0:
+        return 0
+             
+    return 1
+
+def solution3(S):
+    brDict = {  ']':'[',
+                '}':'{',
+                ')':'('}
+
+    seen = []
+
+    for char in S:
+        if char in brDict:
+            if len(seen) == 0 or brDict[char] != seen[-1]:
+                return 0
+            else:
+                seen.pop()
+        else:
+            seen.append(char)
+    if len(seen)==0:
+        return 1
+    else:
+        return 0
+
 if __name__ == '__main__':
     #print(solution("{[()()]}"))
     #print(solution("([)()]"))
