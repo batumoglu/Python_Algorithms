9e70d0e10ed13470d1f757cf3eb4d35618725cf2
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-13 15:28:55 +0000

    Adjusted comments

--- Codility/StoneWall.py
+++ Codility/StoneWall.py
@@ -3,15 +3,12 @@ def solution(H):
     stack = []
      
     for height in H:
-        # remove all blocks that are bigger than my height
         while len(stack) != 0 and stack[-1] > height:
             stack.pop()
          
         if len(stack) != 0 and stack[-1] == height:
-            # we already paid for this size
             pass
         else:
-            # new block is required, push it's size to the stack
             block_cnt += 1
             stack.append(height)
              
