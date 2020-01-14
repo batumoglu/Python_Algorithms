f191a9a283a9c270e53ea3ddda50f0f318c38d08
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-14 12:03:12 +0000

    Golden Leader, candidate

--- Codility/Leader.py
+++ Codility/Leader.py
@@ -27,7 +27,32 @@ def fastLeader(A):
         leader = candidate
     return leader
 
+def goldenLeader(A):
+    n = len(A)
+    size = 0
+    for k in range(n):
+        if size == 0:
+            size += 1
+            value = A[k]
+        else:
+            if value != A[k]:
+                size -= 1
+            else:
+                size += 1
+    candidate = -1
+    if size > 0:
+        candidate = value
+    leader = -1
+    count = 0
+    for k in range(n):
+        if A[k] == candidate:
+            count += 1
+    if count > n/2:
+        leader = candidate
+    return leader
+
 
 if __name__ == '__main__':
     print(slowLeader([5,1,5,2,5,3,5,4,5,6,5,7,5,8,5,9,5]))
     print(fastLeader([5,1,5,2,5,3,5,4,5,6,5,7,5,8,5,9,5]))
+    print(goldenLeader([5,1,5,2,5,3,5,4,5,6,5,7,5,8,5,9,5]))
