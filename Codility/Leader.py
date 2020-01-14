3a2b6937e1ef816f246544581a7a458570af1fd1
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-14 11:55:58 +0000

    Fast Solution, sort, mid

--- Codility/Leader.py
+++ Codility/Leader.py
@@ -14,7 +14,20 @@ def slowLeader(A):
             leader = candidate
     return leader
 
+def fastLeader(A):
+    n = len(A)
+    leader = -1
+    A.sort()
+    candidate = A[int(n/2)]
+    count = 0
+    for i in range(n):
+        if A[i] == candidate:
+            count += 1
+    if count > n/2:
+        leader = candidate
+    return leader
 
 
 if __name__ == '__main__':
     print(slowLeader([5,1,5,2,5,3,5,4,5,6,5,7,5,8,5,9,5]))
+    print(fastLeader([5,1,5,2,5,3,5,4,5,6,5,7,5,8,5,9,5]))
