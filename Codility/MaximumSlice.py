f8bd54a84332cb371af7d7238090f0220d4df3f8
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-15 13:19:40 +0000

    O(n), 2 max in 1 loop

--- Codility/MaximumSlice.py
+++ Codility/MaximumSlice.py
@@ -20,6 +20,15 @@ def quadratic_max_slice(A):
             result = max(result, total)
     return result
 
+def golden_max_slice(A):
+    max_ending = 0
+    max_slice = 0
+    for a in A:
+        max_ending = max(0, max_ending + a)
+        max_slice = max(max_slice, max_ending)
+    return max_slice
+
 if __name__ == '__main__':
     print(slow_max_slice([4,6,-5,1,2,-16,7]))
     print(quadratic_max_slice([4,6,-5,1,2,-16,7]))
+    print(golden_max_slice([4,6,-5,1,2,-16,7]))
