1fefcb24e850631a58fa374549fbcd04dd6e4ff4
Ozkan Batumoglu <batumoglu@hotmail.com>
2020-01-15 13:15:47 +0000

    O(n^2), 2 loops

--- Codility/MaximumSlice.py
+++ Codility/MaximumSlice.py
@@ -4,11 +4,22 @@ def slow_max_slice(A):
 
     for p in range(n):
         for q in range(p, n):
-            sum = 0
+            total = 0
             for i in range(p, q+1):
-                sum += A[i]
-            result = max(result, sum)
+                total += A[i]
+            result = max(result, total)
+    return result
+
+def quadratic_max_slice(A):
+    n = len(A)
+    result = 0
+    for p in range(n):
+        total = 0
+        for q in range(p, n):
+            total += A[q]
+            result = max(result, total)
     return result
 
 if __name__ == '__main__':
     print(slow_max_slice([4,6,-5,1,2,-16,7]))
+    print(quadratic_max_slice([4,6,-5,1,2,-16,7]))
